ed021e14f95d903e7d36024071271d0d6ef4f3b1
Ludovic DEROCHE <ludovic.deroche.thales@gmail.com>
2015-12-04 19:49:31 +0100

    LDER : Dynamic page generation

--- Sampler/www/js/samplere.js
+++ Sampler/www/js/samplere.js
@@ -4,13 +4,21 @@
 'use strict';
 
 function callSound(soundId) {
-  $.post('/api/sounds/play/'+soundId, function(data) {
-     console.log(data);
-  });
-}
-
-function getSounds() {
-    $.post('/api/sounds/info', function (data) {
+    $.post('/api/sounds/play/'+soundId, function(data) {
         console.log(data);
     });
 }
+
+function getSounds() {
+    $.get('/api/sounds/info', function (data) {
+        console.log('get');
+        data.forEach(function(sound) {
+            $('.buttons').append('<a class="btn btn-default col-xs-6 col-sm-3 col-md-2" href="#" role="button" onclick="callSound('+sound.Id+')"><span>'+sound.Name+'</span></a>');
+        });
+
+    });
+}
+
+$().ready(function() {
+    getSounds();
+});
--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe Sampler\Sampler.csproj /t:rebuild /p:Configuration="Debug"
+C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe Sampler\Sampler.csproj /t:build /p:Configuration="Debug" /p:Platform="AnyCPU"
+rem C:\Windows\Microsoft.Net\Framework64\v4.0.30319\MSBuild.exe Sampler.sln /t:rebuild /p:Configuration="Debug" /p:Platform="Any Cpu"
 mkdir output
 del /f  /s /q output\*.*
 xcopy /s /e Sampler\bin\Debug\*.exe .\output\
